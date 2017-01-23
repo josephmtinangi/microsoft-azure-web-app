2a510d59414125cc25fe26df2d7a668f36de4066
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-23 11:32:36 +0000

    Create index.php file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Joseph Mtinangi</title>
+	</head>
+	<body>
+	    
+	    <h1>Joseph Mtinangi</h1>
+	    <p>Full stack developer</p>
+	    <a href="#">Read more &raqua;</a>
+		
+	</body>
+</html>
