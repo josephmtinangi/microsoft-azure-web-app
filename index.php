c8d05353de54f5f8ff0b77c503d7c8f5d52e8dd0
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-23 11:45:26 +0000

    Add footer

--- index.php
+++ index.php
@@ -38,6 +38,10 @@
 	    <!--site-main-->
 	    
 	    <!--site-footer-->
+	    <footer>
+	    	Copyright &copy; 2016 All Rights Reserved
+	    </footer>
+	    
 	    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		
