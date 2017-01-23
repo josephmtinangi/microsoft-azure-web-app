3272bc63e2577db4444181684a924eb4152ee403
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-23 12:13:24 +0000

    Add responsive navbar

--- index.php
+++ index.php
@@ -4,30 +4,55 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Joseph Mtinangi</title>
+		
+		<title>SmartTuts</title>
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" />
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 	</head>
 	<body>
-	    <div class="navbar">
-	    	<div class="container-fluid">
-	    		<a class="navbar-brand" href="#">Joseph Mtinangi</a>
-	    		<ul class="nav navbar-nav">
-	    			<li class="active">
-	    				<a href="#">Home</a>
-	    			</li>
-	    			<li>
-	    				<a href="#">About</a>
-	    			</li>
-	    		</ul>
-	    	</div>
-	    </div>	    
+	    
+	<nav class="navbar navbar-inverse" role="navigation">
+    	<div class="container-fluid">
+    		<!-- Brand and toggle get grouped for better mobile display -->
+    		<div class="navbar-header">
+    			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+    				<span class="sr-only">Toggle navigation</span>
+    				<span class="icon-bar"></span>
+    				<span class="icon-bar"></span>
+    				<span class="icon-bar"></span>
+    			</button>
+    			<a class="navbar-brand" href="#">ST</a>
+    		</div>
+    
+    		<!-- Collect the nav links, forms, and other content for toggling -->
+    		<div class="collapse navbar-collapse navbar-ex1-collapse">
+    			<ul class="nav navbar-nav">
+    				<li class="active"><a href="#">Browse</a></li>
+    				<li><a href="#">Courses</a></li>
+    			</ul>
+    			<form class="navbar-form navbar-left" role="search">
+    				<div class="form-group">
+    					<input type="text" class="form-control" placeholder="Search">
+    				</div>
+    				<button type="submit" class="btn btn-default">Search</button>
+    			</form>
+    			<ul class="nav navbar-nav navbar-right">
+    				<li><a href="#">Go Pro</a></li>
+    				<li><a href="#">Sign in</a></li>
+    				<li><a href="#">Create Account for Free</a></li>
+    			</ul>
+    		</div><!-- /.navbar-collapse -->
+    	</div>
+    </nav>	    
+	    	    
 	    <!--site header-->
 	    <header>
 	    	<div class="jumbotron">
-	    		<div class="container">
-	    			<h1>Joseph Mtinangi</h1>
-	    			<p>I am learning Microsoft Windows Server</p>
+	    		<div class="container text-center">
+	    			<h1>Smart Tutorials</h1>
+	    			<p>The place to be.</p>
 	    			<p>
 	    				<a class="btn btn-primary btn-lg">Read more &raquo;</a>
 	    			</p>
@@ -38,7 +63,7 @@
 	    
 	    <!--site-main-->
 	    <main>
-	    	
+	    		    	
 	    </main>
 	    
 	    <!--site-footer-->
